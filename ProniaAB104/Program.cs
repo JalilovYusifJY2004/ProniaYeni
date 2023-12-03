922b1131f6b5dceb6b462391a963357e97daaf26
JalilovYusifJY2004 <yusifcelilli2004@gmail.com>
2023-12-03 21:01:33 +0400

    ProniaYeni

---
+++ ProniaAB104/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using ProniaAB104.DAL;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+   opt.UseSqlServer(builder.Configuration.GetConnectionString("Default"))
+);
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllerRoute(
+    "default",
+    "{area:exists}/{controller=home}/{action=index}/{id?}"
+    );
+app.MapControllerRoute(
+    "default",
+    "{controller=home}/{action=index}/{id?}"
+    );
+
+app.Run();
