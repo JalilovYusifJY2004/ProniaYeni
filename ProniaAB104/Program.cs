97fc8ea64ab7b45747a05475ab1ea77e54af25af
JalilovYusifJY2004 <yusifcelilli2004@gmail.com>
2023-12-04 22:15:45 +0400

    Identity

--- ProniaAB104/Program.cs
+++ ProniaAB104/Program.cs
@@ -1,13 +1,34 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ProniaAB104.DAL;
+using ProniaAB104.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(opt =>
    opt.UseSqlServer(builder.Configuration.GetConnectionString("Default"))
 );
-var app = builder.Build();
+builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+{
+    options.Password.RequiredLength = 128;
+    options.Password.RequireNonAlphanumeric=false;
+    options.Password.RequireDigit=true;
+    options.Password.RequireLowercase=true;
+    options.Password.RequireUppercase=true;
 
+    options.User.RequireUniqueEmail=true;
+
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+}).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+
+
+
+
+var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization ();
 app.UseStaticFiles();
 app.UseRouting();
 app.MapControllerRoute(
